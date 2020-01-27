f3b57687abd89bd8f7ba5af52f8c2e76cb8b10fe
Sean Sundberg <seansund@us.ibm.com>
2020-01-27 09:03:51 +0100

    Updates index script to include pipelines container in yaml

--- ci/index.sh
+++ ci/index.sh
@@ -6,6 +6,6 @@ yq new name garage-pipelines > index.yaml
 find . -name "*.tar.gz" | while read -r file; do
   name=$(basename "${file}" | cut -f 1 -d '.')
   path=$(echo "${file}" | sed "s/^[.]\///g")
-  yq w -i index.yaml "${name}.url" "${REPO_PATH}/${path}"
-  yq w -i index.yaml "${name}.name" "${name}"
+  yq w -i index.yaml "pipelines.${name}.url" "${REPO_PATH}/${path}"
+  yq w -i index.yaml "pipelines.${name}.name" "${name}"
 done
