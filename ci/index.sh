b85fd0581322bdfe50b88d533d0ddcab87077133
Sean Sundberg <seansund@us.ibm.com>
2020-04-11 16:43:21 -0500

    Incorporates versioning into garage-pipelines
    - Updates the ci scripts to handle version numbers
    - Sets initial/default version number to 0.0.0 in helm charts
    - Adds pipeline.yaml in each pipeline to provide metadata (name, version, build-tool, and runtime)

--- ci/index.sh
+++ ci/index.sh
@@ -5,10 +5,38 @@ REPO_PATH="$2"
 
 cd "${PUBLISH_DIR}"
 
-yq new name garage-pipelines > index.yaml
-find . -name "*.tar.gz" | while read -r file; do
-  name=$(basename "${file}" | cut -f 1 -d '.')
+index=0
+last_name=""
+
+yq new version v2 > index.yaml
+find . -name "*.tar.gz" | sort -r -V | while read -r file; do
+  branch=$(basename $(dirname "${file}"))
+  name=$(basename "${file}" | sed -E "s/^([a-z-]+)-([0-9]+[.][0-9]+[.][0-9]+).tar.gz/\1/g")
+  version=$(basename "${file}" | sed -E "s/^([a-z-]+)-([0-9]+[.][0-9]+[.][0-9]+).tar.gz/\2/g")
+
   path=$(echo "${file}" | sed "s/^[.]\///g")
-  yq w -i index.yaml "pipelines.${name}.url" "${REPO_PATH}/${path}"
-  yq w -i index.yaml "pipelines.${name}.name" "${name}"
+
+  if [[ "$last_name" != "${name}" ]]; then
+    index=0
+  else
+    index=$((index+1))
+  fi
+
+  if [[ $index -eq 0 ]]; then
+    yq w -i index.yaml "pipelines.${name}.url" "${REPO_PATH}/${path}"
+    yq w -i index.yaml "pipelines.${name}.name" "${name}"
+    yq w -i index.yaml "pipelines.${name}.version" "${version}"
+
+    yq w -i index.yaml "branches.${branch}.${name}[+].url" "${REPO_PATH}/${path}"
+    yq w -i index.yaml "branches.${branch}.${name}[$index].name" "${name}"
+    yq w -i index.yaml "branches.${branch}.${name}[$index].version" "latest"
+
+    index=$((index+1))
+  fi
+
+  yq w -i index.yaml "branches.${branch}.${name}[+].url" "${REPO_PATH}/${path}"
+  yq w -i index.yaml "branches.${branch}.${name}[$index].name" "${name}"
+  yq w -i index.yaml "branches.${branch}.${name}[$index].version" "${version}"
+
+  last_name=$name
 done
--- ci/package.sh
+++ ci/package.sh
@@ -5,13 +5,35 @@ BASE_DIR=$(cd ${SCRIPT_PATH}/..; pwd -P)
 
 PUBLISH_DIR="$1"
 
+TMP_DIR="./tmp"
+mkdir -p $TMP_DIR
+
 DIR_PATH="stable"
 
 for item in $DIR_PATH; do
-  find ${BASE_DIR}/${item} -mindepth 1 -maxdepth 1 | while read path; do
-    pipeline=$(basename "${path}")
+  find "${BASE_DIR}/${item}" -mindepth 1 -maxdepth 1 | while read -r path; do
+    pipeline=$(yq r "${path}/pipeline.yaml" name)
+    version=$(yq r "${path}/pipeline.yaml" version)
+
+    TMP_PATH="${TMP_DIR}/${pipeline}"
+    mkdir -p "${TMP_PATH}"
+    cp -R "${path}"/* "${TMP_PATH}"
+
+    # Add the version number to the top of the Jenkinsfile
+    echo "def pipelineVersion='${version}'" > "${TMP_PATH}/Jenkinsfile"
+    echo 'println "Pipeline version: ${pipelineVersion}"' >> "${TMP_PATH}/Jenkinsfile"
+    cat $(find "${path}" -name Jenkinsfile) >> "${TMP_PATH}/Jenkinsfile"
+
+    # update the helm chart version number
+    yq w --inplace $(find "${TMP_PATH}" -name Chart.yaml) version "${version}"
+
     mkdir -p "${BASE_DIR}/${PUBLISH_DIR}/${item}"
-    cd "${path}"
-    tar czf "${BASE_DIR}/${PUBLISH_DIR}/${item}/${pipeline}.tar.gz" .
+    cd "${TMP_PATH}"
+
+    tar czf "${BASE_DIR}/${PUBLISH_DIR}/${item}/${pipeline}-${version}.tar.gz" .
+
+    cd - > /dev/null
   done
 done
+
+rm -rf ${TMP_DIR}
