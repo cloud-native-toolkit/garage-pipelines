eff69cf3734d2e0dbc3e64dd4d1cb15c7f0bc359
Sean Sundberg <seansund@us.ibm.com>
2020-04-15 15:53:05 -0500

    Updates build logic

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-set +x
+TARGET_BRANCH="$1"
+PUBLISH_DIR="$2"
 
-git ch gh-pages
-git pull
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-cp ./tmp/stable/* ./stable
-rm -rf ./tmp
+commit_website_files() {
+  git checkout -b "${TARGET_BRANCH}" --track "origin/${TARGET_BRANCH}"
+  cp -R "${PUBLISH_DIR}"/* .
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+#  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+#  git push --quiet --set-upstream origin-pages gh-pages
+   git push
+}
+
+setup_git
+commit_website_files
+upload_files
